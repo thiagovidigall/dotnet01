93a32dabe1b31a207a87212490eaeb0cf510f2d9
thiagovidigall <thiagovidigall@gmail.com>
2024-09-04 12:36:00 -0300

    add method

--- DevBlueStore/src/services/DBlue.Carrinho.API/WeatherForecast.cs
+++ DevBlueStore/src/services/DBlue.Carrinho.API/WeatherForecast.cs
@@ -11,5 +11,7 @@ namespace DBlue.Carrinho.API
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+
+        public string Summary2 { get; set; }
     }
 }
