f0fd0db238fe4fa380711e02b7066d26885621df
thiago.vidigal <thiago.vidigal@ueg.br>
2024-11-22 09:47:01 -0300

    add carrinho model add carrinho context add startup, appsettings e launchSettings

---
+++ DevBlueStore/src/services/DBlue.Carrinho.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using DBlue.Carrinho.API.Data;
+//using DBlue.WebAPI.Core.Usuario;
+
+namespace DBlue.Carrinho.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            //services.AddScoped<IAspNetUser, AspNetUser>();
+            services.AddScoped<CarrinhoContext>();
+        }
+    }
+}
